61b341f130695b80c0655cfd6a7bb0455c115d42
SharadRawat <rawat.33@osu.edu>
2020-06-13 19:16:54 +0100

    Add working code to test Ultrasonic.

--- l298_motor_control_module/motor_controller.py
+++ l298_motor_control_module/motor_controller.py
@@ -7,7 +7,8 @@ def init():
     gpio.setup(22, gpio.OUT)
     gpio.setup(23, gpio.OUT)
     gpio.setup(24, gpio.OUT)
-    
+
+
 
 def forward(sec):
     init()
@@ -38,33 +39,43 @@ def left_turn(sec):
     time.sleep(sec)
     gpio.cleanup()
     
-    
-def right_turn(sec):
+def forward_right_turn(sec):
     init()
     gpio.output(17, False)
     gpio.output(22, True)
-    gpio.output(23, False)
+    gpio.output(23, True)
     gpio.output(24, True)
     time.sleep(sec)
-    gpio.cleanup()
+    gpio.cleanup()  
+
     
     
-seconds = 5
+seconds = 2
+
+time.sleep(seconds)
 print("forward")
-forward(seconds)
-time.sleep(seconds)
+forward(seconds-1)
+time.sleep(seconds-1)
 
+#forward_right_turn(seconds-4)
+
+reverse(seconds-1)
+
+
+#forward_right_turn(seconds-4.5)
 print("reverse")
-reverse(seconds)
-time.sleep(seconds)
-
+#reverse(seconds-4)
+#time.sleep(seconds-4.5)
+'''
 print("left_turn")
 left_turn(seconds)
 time.sleep(seconds)
 
-print("right_turn")
-right_turn(seconds)
-time.sleep(seconds)
 
+print("right_turn")
+forward_right_turn(seconds-2)
+forward(seconds)
+time.sleep(seconds)
+'''
 
 ### TODO Map the time with how much should the robot rotate
